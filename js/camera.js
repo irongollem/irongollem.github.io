1e1a250a150c4a533a6889aac678bfda6cf51bb8
irongollem <Koningin62616>
2017-03-25 01:51:21 +0100

    voorbereiding voor camera en upload

--- js/camera.js
+++ js/camera.js
@@ -1,8 +1,30 @@
 var camera = document.getElementById('camera');
 var frame = document.getElementById('frame');
+var uploadButton = document.getElementById('upload-button');
+var client = new XMLHttpRequest();
 
-camera.addEventListener('change', function(e) {
-  var file = e.target.files[0];
-  // Do something with the image file.
-  frame.src = URL.createObjectURL(file);
-});
+camera.addEventListener('change', loadFotoToPreview);
+uploadButton.addEventListener('click', uploadFoto);
+client.addEventListener('readystatechange', sendSuccessMessage);
+
+function uploadFoto(e) {
+    var formData = new FormData();
+
+    formData.append('foto', camera.files[0]);
+    formData.append('title', 'Untitled');
+
+    client.open('post', REPLACE_WITH_URL, true);
+    client.setRequestHeader('Content-Type', 'multipart/form-data');
+    client.send(formData);
+}
+
+function loadFotoToPreview(e) {
+    var file = e.target.files[0];
+    frame.src = URL.createObjectURL(file);
+}
+
+function sendSuccessMessage() {
+    if (client.readyState == 4 && client.status == 200) {
+        alert(client.statusText);
+    }
+}
