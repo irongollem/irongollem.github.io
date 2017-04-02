90c3e8ab7d83b5c8c949efd82fe9cb2daa93be1b
irongollem <Koningin62616>
2017-04-02 22:48:27 +0200

    polling logic

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,13 +1,23 @@
-var imageArray
+var imageArray;
+var lastId;
 
-(function poll() {
-  var url = "https://zulfiqar.nl/kelvinrianka/getImages.php?key=jS8qpkqNoGiANjE49MHuddyX4rsnaku9LvkSmHBt";
-   setTimeout(function() {
-       $.ajax({
-         url: url, success: function(data) {
-            sales.setValue(data.value);
-       },
-       dataType: "json",
-        complete: poll });
+// Fill the image array every 30.000 ms with the new picture meta data
+(function poll(lastId) {
+    var extention = lastId ? "lastId=" + lastId : "";
+    var url = "https://zulfiqar.nl/kelvinrianka/getImages.php?key=jS8qpkqNoGiANjE49MHuddyX4rsnaku9LvkSmHBt" + extension;
+    setTimeout(function() {
+        $.ajax({
+            url: url,
+            dataType: "json",
+            complete: poll.bind(null, lastId),
+            success: function(data) {
+                data.forEach(function(entry) {
+                    imageArray.push(entry);
+                });
+                lastId = imageArray[imageArray.length -1].id;
+            },
+        });
     }, 30000);
 })();
+
+// The actual slide show logic using the imageArray as reference
